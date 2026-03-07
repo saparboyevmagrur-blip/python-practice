3a4b3042f87abda61ff09527d2eb5b3f13ba6adf
saparboyevmagrur-blip <saparboyevmagrur.com>
2026-03-07 16:22:59 +0500

    practice: while loop

---
+++ test.py
@@ -0,0 +1,4 @@
+n = int(input())
+sonlar = list(range(2, n + 1, 2))
+yigindi = sum(sonlar)
+print(yigindi)
--- while-loop-practice.py
+++ while-loop-practice.py
@@ -1,8 +1,30 @@
-savol = "Sevgan kitobingizni kiriting"
-savol += "(barcha kitoblarni kiritib bo'lgach 'exit' deb yozing): "
+# 1.
+# savol = "Sevgan kitobingizni kiriting"
+# savol += "(barcha kitoblarni kiritib bo'lgach 'exit' deb yozing): "
+
+# while True:
+#     kitob = input(savol)
+#     if kitob == 'exit':
+#         break
+# print('Rahmat!')  
+
+# 2.
+savol = "Yoshingiz nechida? Yoshingizni kiriting, sizga chipta narxini chiqarib beraman: "
 
 while True:
-    kitob = input(savol)
-    if kitob == 'exit':
+    print("Agar 'exit' va 'quit' deb yozsangiz, dastur to'xtaydi")
+    yosh = input(savol)
+    if yosh == 'exit' or yosh == 'quit':
+        print("Dastur to'xtadi")
         break
-print('Rahmat!')      
+
+    yosh = int(yosh)
+    if yosh < 7:
+        print("Sizga 2000 so'm")
+    elif yosh >= 7 and yosh < 18:
+        print("Sizga 3000 so'm")
+    elif yosh >= 18 and yosh < 65:
+        print("Sizga 10000 so'm")
+    else:
+        print("Sizga bepul")
+
